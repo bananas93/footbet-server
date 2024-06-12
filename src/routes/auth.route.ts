d76f9b21cc6c5e90c83005241d914fc7c8b3b1b4
David Amerov <davidam@varyence.com>
2024-06-12 23:32:02 +0200

    chore: Update BASE_URL in .env.develop and callbackURL in auth.route.ts

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -14,7 +14,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/api/auth/callback',
+      callbackURL: `${process.env.BASE_URL}/api/auth/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
